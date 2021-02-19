2d72cce4c32cfc2f1d9506c83ec7d1fe9d0efb2b
adityakeerthi <54603000+Aditya-Keerthi@users.noreply.github.com>
2021-02-18 22:23:02 -0500

    Making revision to the workplace, and getting ready for application testing

--- Tk.py
+++ Tk.py
@@ -1,28 +1,26 @@
-# import tkinter as tk
-
-# class Application(tk.Frame):
-#     def __init__(self, master=None):
-#         super().__init__(master)
-#         self.master = master
-#         self.pack()
-#         self.create_widgets()
-
-#     def create_widgets(self):
-#         self.hi_there = tk.Button(self)
-#         self.hi_there["text"] = "Hello World\n(click me)"
-#         self.hi_there["command"] = self.say_hi
-#         self.hi_there.pack(side="top")
-
-#         self.quit = tk.Button(self, text="QUIT", fg="red",
-#                               command=self.master.destroy)
-#         self.quit.pack(side="bottom")
-
-#     def say_hi(self):
-#         print("hi there, everyone!")
-
-# root = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
-
-for i in range(1):
-    print(i)
+# importing tkinter 
+from tkinter import * 
+from tkinter import ttk 
+from tkinter.messagebox import askyesno 
+  
+# creating root 
+root = Tk() 
+root.geometry('200x100') 
+  
+input_text = StringVar() 
+  
+# This class is used to add styling 
+# to any widget which are available 
+style = ttk.Style() 
+style.configure('TEntry', foreground = 'green') 
+  
+entry1 = ttk.Entry(root, textvariable = input_text, justify = CENTER, 
+                                     font = ('courier', 15, 'bold'))    
+entry1.focus_force() 
+entry1.pack(side = TOP, ipadx = 30, ipady = 10) 
+  
+save = ttk.Button(root, text = 'Save', command = lambda : askyesno( 
+                                 'Confirm', 'Do you want to save?')) 
+save.pack(side = TOP, pady = 10) 
+  
+root.mainloop() 
